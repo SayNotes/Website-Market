11ef3972f9af88f01561328f91441e1361d3cf6c
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-06-17 09:25:45 +0700

    Menambahkan file dasar yang dibutuhkan Website

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// class Latihan {
+
+//     constructor(name1, name2){
+//         this.firstName = name1
+//         this.lastName = name2
+//         this.sayHello = function() {
+//             console.log(`Hi! My name is ${name1} ${name2}`)
+//         }
+//         return this.sayHello()
+//     }
+// }
+
+// const coba = new Latihan('Sultan', 'Faiz');
+// console.log(coba)
+
+
+// class Copy extends Latihan {
+// }
+
+// const coba1 = new Copy()
+// console.log(coba1)
