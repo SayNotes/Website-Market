b207dd05ff6cd379b7c0cf8cf6e2c8af3691e4f5
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-08-02 13:21:11 +0700

    Menambahkan foto untuk card dan script js untuk menghitung jumlah harga

--- js/script.js
+++ js/script.js
@@ -13,17 +13,11 @@
 // const coba = new Latihan('Sultan', 'Faiz');
 // console.log(coba)
 
-
 // class Copy extends Latihan {
 // }
 
 // const coba1 = new Copy()
 // console.log(coba1)
-
-
-
-
-
 
 // const text = "Hello, world!"; // Teks yang ingin ditampilkan
 // const typingEffectElement = document.getElementById("typing-effect");
@@ -39,3 +33,77 @@
 // }
 
 // type(); // Mulai efek ketikan saat halaman dimuat
+const priceLocal = 80;
+let input = document.getElementById("numericInput");
+let demand = document.querySelector("h1#result");
+
+function formatNumberWithCommas(number) {
+    return number.toLocaleString();
+};
+
+function increment() {
+    input.value = Number(input.value) + 1;
+    demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
+};
+
+function decrement() {
+    if (input.value > 0) {
+        input.value = Number(input.value) - 1;
+    } else if (input.value < 0) {
+        input.value = 0;
+    };
+    demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
+};
+
+input.addEventListener("input", function () {
+    demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
+});
+
+
+function buttonClicked() {
+    alert("Tombol Submit Ditekan!");
+};
+
+
+
+class CounterIteratorResult {
+    constructor (value, done) {
+        this.value = value;
+        this.done = done;
+    }
+}
+
+class CounterIterator {
+    constructor (value, max) {
+        this.value = value;
+        this.max = max;
+    }
+
+    next() {
+        try {
+            if (this.value > this.max) {
+                return new CounterIteratorResult(this.value, true);
+            } else {
+                return new CounterIteratorResult(this.value, false);
+            }
+        } finally {
+            this.value++;
+        }
+    }
+}
+
+
+// class Counter {
+//     constructor (value, done) {
+//         this.value = value;
+//         this.done = done;
+//     }
+
+//     [Symbol.iterator]() {
+//         return new CounterIterator(this.value, this.max);
+//     }
+// }
+
+// const counter = new Counter(1, 10)
+// for (const element of counter) {
+//     console.info(element);
