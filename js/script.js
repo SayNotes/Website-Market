b45057276bab7a9b04892ae89c0c449d378382f3
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-09-13 23:57:26 +0700

    Menambahkan popUp, Menambahkan footer-page, dan lain-lain

--- js/script.js
+++ js/script.js
@@ -1,59 +1,55 @@
-const priceLocal = 100;
-let input = document.getElementById("numericInput");
-let demand = document.querySelector("h1#result");
+const navbar = document.getElementById("search-bar");
+const homePage = document.getElementById("home-page");
+const popUp = document.getElementById("welcome-user");
 
-function formatNumberWithCommas(number) {
-    return number.toLocaleString();
-};
+function closePopUp() {
+  popUp.style.display = 'none';
+}
 
-function increment() {
-    input.value = Number(input.value) + 1;
-    if (input.value < 0) {
-        input.value = 0
-    };
-    demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
-};
+// const priceLocal = 100;
+// let input = document.getElementById("numericInput");
+// let demand = document.querySelector("h1#result");
 
-function decrement() {
-    if (input.value > 0) {
-        input.value = Number(input.value) - 1
-    } else if (input.value < 0) {
-        input.value = 0
-    };
-    demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
-};
+// function formatNumberWithCommas(number) {
+//   return number.toLocaleString();
+// }
 
-input.addEventListener("input", function () {
-    demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
-});
+// function increment() {
+//   input.value = Number(input.value) + 1;
+//   if (input.value < 0) {
+//     input.value = 0;
+//   }
+//   demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
+// }
 
+// function decrement() {
+//   if (input.value > 0) {
+//     input.value = Number(input.value) - 1;
+//   } else if (input.value < 0) {
+//     input.value = 0;
+//   }
+//   demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
+// }
+
+// input.addEventListener("input", function () {
+//   demand.innerText = "Rp " + formatNumberWithCommas(input.value * priceLocal);
+// });
 
 function buttonClicked() {
-    if (input.value > 0) {
-        alert("Pesanan telah masuk");
-    } else if (input.value < 0) {
-        alert("Pesanan tidak dapat masuk");
-    }
-};
+  if (input.value > 0) {
+    alert("Pesanan telah masuk");
+  } else if (input.value < 0) {
+    alert("Pesanan tidak dapat masuk");
+  }
+}
 
-const navBar = document.getElementById("nav");
-const homePage = document.getElementById("home-page");
-const heightNavbar = navBar.clientHeight;
-
-window.addEventListener("scroll", function() {
-
-    if (window.scrollY > heightNavbar) {
-        navBar.style.position = "fixed";
-        navBar.style.top = 0;
-        navBar.style.left = 0;
-        navBar.style.zIndex = 999;
-        navBar.style.animationName = "upToDown";
-        navBar.style.animationDuration = "300ms";
-        navBar.style.animationTimingFunction = "ease in";
-        homePage.style.marginTop = `${heightNavbar}px`
-    } else {
-        navBar.style.position = "static";
-        navBar.style.animation = "";
-        homePage.style.marginTop = 0;
-    }
+window.addEventListener("scroll", function () {
+  if (window.scrollY > navbar.clientHeight) {
+    navbar.classList.add("nav-animation");
+    navbar.style.animationName = "fade-in";
+    homePage.style.marginTop = `${navbar.clientHeight}px`;
+  } else if (window.scrollY <= navbar.clientHeight ){
+    navbar.classList.remove("nav-animation");
+    homePage.style.marginTop = 0;
+  }
 });
