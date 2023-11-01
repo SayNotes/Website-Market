a3cfeba7559da44987fe6aa2f04f8bfcaabf9a70
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-11-01 20:47:30 +0700

    Menghapus file tidak penting dan memindahkan beberapa file menjadi lebih rapi

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,6 @@
 //   return number.toLocaleString();
 // }]
 
-const cardList = document.getElementsByClassName('list-card');
 const cards = document.getElementsByClassName('card');
 const btnPlus = document.getElementsByClassName('btn-plus');
 const btnMinus = document.getElementsByClassName('btn-minus');
@@ -21,19 +20,32 @@ const input = document.getElementsByClassName('inputAmount');
 //   event.target.value = value;
 // });
 
+for (let i = 0; i < btnPlus.length; i++) {
+  let btnPlusIndex = btnPlus[i]
 
-
-function increment() {
-  let valueNumber = parseInt(input.inputAmount.value) || 0;
-  input.inputAmount.value = valueNumber + 100;
+  btnPlusIndex.addEventListener('onclick', () => {
+    let getInputIndex = input[i]
+    let valueInput = parseInt(getInputIndex.value) || 0;
+    getInputIndex.value = valueInput + 100
+    console.log(getInputIndex)
+  })
+  
 }
 
-function decrement() {
-  let value = parseInt(input.inputAmount.value) || 0;
-  if (value > 0) {
-    input.inputAmount.value = value - 100;
-  }
-}
+
+
+  // btnMinus[getIndex].addEventListener('onclick', () => {
+  //   decrement()
+  // });
+
+  // btnPlus[getIndex].addEventListener('onclick', () => {
+  //   increment()
+  // });
+  // console.log(btnMinus[getIndex])
+
+
+
+
 
 function buttonClicked() {
   alert(`Pesanan senilai Rp.${input.inputAmount.value} telah masuk`)
@@ -51,36 +63,45 @@ function buttonClicked() {
 // });
 
 
+{
+  const listCategory = document.getElementById("list-category");
+  let isDragging = false;
+  let startX = 0;
 
-const listCategory = document.getElementById("list-category");
-let isDragging = false;
-let startX = 0;
+  const startDrag = (e) => {
+    isDragging = true;
+    listCategory.classList.add("scroll-auto");
+    startX = e.clientX || e.touches[0].clientX;
+  };
 
-const startDrag = (e) => {
-  isDragging = true;
-  listCategory.classList.add("scroll-auto");
-  startX = e.clientX || e.touches[0].clientX;
-};
-console.log(startX);
+  const drag = (e) => {
+    if (!isDragging) return;
+    const currentX = e.clientX || e.touches[0].clientX;
+    const diffX = startX - currentX;
+    listCategory.scrollLeft += diffX;
+    startX = currentX;
+  };
 
-const drag = (e) => {
-  if (!isDragging) return;
-  const currentX = e.clientX || e.touches[0].clientX;
-  const diffX = startX - currentX;
-  listCategory.scrollLeft += diffX;
-  startX = currentX;
-};
+  const stopDrag = () => {
+    isDragging = false;
+    listCategory.classList.remove("scroll-auto");
+  };
 
-const stopDrag = () => {
-  isDragging = false;
-  listCategory.classList.remove("scroll-auto");
-};
+  listCategory.addEventListener("mousedown", startDrag);
+  listCategory.addEventListener("mousemove", drag);
+  document.addEventListener("mouseup", stopDrag);
 
-listCategory.addEventListener("mousedown", startDrag);
-listCategory.addEventListener("mousemove", drag);
-document.addEventListener("mouseup", stopDrag);
+  listCategory.addEventListener("touchstart", startDrag);
+  listCategory.addEventListener("touchmove", drag);
+  document.addEventListener("touchend", stopDrag);
+}
 
-listCategory.addEventListener("touchstart", startDrag);
-listCategory.addEventListener("touchmove", drag);
-document.addEventListener("touchend", stopDrag);
+// class MyClass {
 
+//   sayWorld() {
+//     console.log('Hello World');
+//   }
+
+// };
+
+// const example = new MyClass()
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         gradientDarken: 'linear-gradient(180deg, rgba(100,116,139,0.15) 0%, rgba(255,255,255,0) 65%)'
       },
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif',]
+        poppins: ['Poppins', 'sans-serif']
       },
     },
     plugins: [],
