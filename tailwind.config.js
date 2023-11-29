cb612903efd633ae550f638bffdc78997f684167
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-11-29 19:00:31 +0700

    Mengubah tampilan website menjadi lightmode dan memperbaiki .card

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,12 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        gradientDarken: 'linear-gradient(180deg, rgba(100,116,139,0.15) 0%, rgba(255,255,255,0) 65%)'
-      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif']
       },
     },
-    plugins: [],
+    plugins: {
+      '@tailwindcss/jit': {}
+    },
   },
 };
